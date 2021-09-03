dfdf5b54dbd59abfa5651f2f14db199a861fcf51
edilbertojj65 <edilbertojj65@gmail.com>
2021-09-02 20:12:17 -0400

    Completed

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.belongs_to :taxi
+      t.belongs_to :passengers
       t.timestamps
     end
   end
